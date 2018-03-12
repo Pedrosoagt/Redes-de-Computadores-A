d6e7a910bc7bc49fde8557b528734f269088b6ef
Bruno Pedroso <pedrosoagt@gmail.com>
2018-03-12 18:03:24 -0300

    organiza pastas

--- Tarefa2/clientcp.c
+++ Tarefa2/clientcp.c
@@ -6,13 +6,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-#define NAME_SIZE 19
-#define MG_SIZE 79
+#define NAME_SIZE 20
+#define MSG_SIZE 80
+#define ANS_SIZE 1250
 
 struct msg{
 	char name[NAME_SIZE];
-	char mensage[MG_SIZE];
+	char message[MSG_SIZE];
+	char answer[ANS_SIZE];
 	int type;
 };
 
@@ -23,110 +26,138 @@ void main(argc, argv)
 int argc;
 char **argv;
 {
-    unsigned short port;       
-    char sendbuf[12];              
-    char recvbuf[12];              
-    struct hostent *hostnm;    
-    struct sockaddr_in server; 
-    int s;
-    int type_clt=0;
-    struct msg envio;                     
+	unsigned short port;
+	char sendbuf[12];
+	char recvbuf[12];
+	char msg[80];
+	struct hostent *hostnm;
+	struct sockaddr_in server;
+	int s;
+	int type_clt=0;
+	struct msg envio;
 
-    /*
-     * O primeiro argumento (argv[1]) é o hostname do servidor.
-     * O segundo argumento (argv[2]) é a porta do servidor.
-     */
-    if (argc != 3)
-    {
-        fprintf(stderr, "Use: %s hostname porta\n", argv[0]);
-        exit(1);
-    }
+	/*
+	 * O primeiro argumento (argv[1]) ï¿½ o hostname do servidor.
+	 * O segundo argumento (argv[2]) ï¿½ a porta do servidor.
+	 */
+	if (argc != 3) {
+		fprintf(stderr, "Use: %s hostname porta\n", argv[0]);
+		exit(1);
+	}
 
-    /*
-     * Obtendo o endereço IP do servidor
-     */
-    hostnm = gethostbyname(argv[1]);
-    if (hostnm == (struct hostent *) 0)
-    {
-        fprintf(stderr, "Gethostbyname failed\n");
-        exit(2);
-    }
-    port = (unsigned short) atoi(argv[2]);
+	/*
+	 * Obtendo o endereï¿½o IP do servidor
+	 */
+	hostnm = gethostbyname(argv[1]);
+	if (hostnm == (struct hostent *) 0) {
+		fprintf(stderr, "Gethostbyname failed\n");
+		exit(2);
+	}
+	port = (unsigned short) atoi(argv[2]);
 
-    /*
-     * Define o endereço IP e a porta do servidor
-     */
-    server.sin_family      = AF_INET;
-    server.sin_port        = htons(port);
-    server.sin_addr.s_addr = *((unsigned long *)hostnm->h_addr);
+	/*
+	 * Define o endereï¿½o IP e a porta do servidor
+	 */
+	server.sin_family      = AF_INET;
+	server.sin_port        = htons(port);
+	server.sin_addr.s_addr = *((unsigned long *)hostnm->h_addr);
 
-    /*
-     * Cria um socket TCP (stream)
-     */
-    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        perror("Socket()");
-        exit(3);
-    }
+	/*
+	 * Cria um socket TCP (stream)
+	 */
+	if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("Socket()");
+		exit(3);
+	}
 
-    /* Estabelece conexão com o servidor */
-    if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0)
-    {
-        perror("Connect()");
-        exit(4);
-    }
-    /*Menu Inicial*/
-    printf("Opções:\n 1-Cadastrar mensagem\n 2-Ler mensagens\n 3 - Apagar mensagens\n 4 - Sair da Aplicação\n");
-    
-    /*receber e tratar a seleção*/
-    scanf("%i", &type_clt);
-    
-    switch(type_clt){
-    	case 1:
-		printf("Usuario:");
-		scanf("%s", envio.name);
-		printf("\nMensagem:");
-		scanf("%s", envio.mensage);
-		envio.type = 1;
-    		break;
-	case 2:
-		strcpy(sendbuf, "Ler");
-		break;
-	case 3:
-		strcpy(sendbuf, "Apagar");
-		break;
-	case 4:
-		strcpy(sendbuf, "Sair");
-		break;
-	default:
-		strcpy(sendbuf, "Requisição");
-		break;
-    }
-    
+	/* Estabelece conexï¿½o com o servidor */
+	if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0) {
+		perror("Connect()");
+		exit(4);
+	}
 
+	while(1) {
+		/*Menu Inicial*/
+		printf("Opcoes:\n1- Cadastrar mensagem\n2- Ler mensagens\n3- Apagar mensagens\n4- Sair\n");
 
-    /* Envia a mensagem no buffer de envio para o servidor */
-    if (send(s, &envio, sizeof(envio), 0) < 0)
-    {
-        perror("Send()");
-        exit(5);
-    }
-    printf("Mensagem enviada ao servidor: %s\n", envio.mensage);
+		/*receber e tratar a seleï¿½ï¿½o*/
+		scanf("%i", &type_clt);
 
-    /* Recebe a mensagem do servidor no buffer de recepção */
-    if (recv(s, &envio, sizeof(envio), 0) < 0)
-    {
-        perror("Recv()");
-        exit(6);
-    }
-    printf("Mensagem recebida do servidor: %s\n", envio.mensage);
+		// inicializacao padrao de valores da struct
+		strcpy(envio.name, "empty");
+		strcpy(envio.message, "empty");
 
-    /* Fecha o socket */
-    close(s);
+		switch(type_clt) {
+			case 1:
+				strcpy(msg, "");
+				
+				// Nome
+				puts("Usuario (mÃ¡ximo 19 caracteres):");
+				fpurge(stdin);
+				fgets(envio.name, NAME_SIZE, stdin);
+				strtok(envio.name, "\n");
+				// Mensagem
+				puts("Mensagem (mÃ¡ximo 79 caracteres):");
+				fpurge(stdin);
+				fgets(envio.message, MSG_SIZE, stdin);
+				envio.type = 1;
+			break;
+			case 2:
+				strcpy(msg, "Mensagens cadastradas:");
+				envio.type = 2;
+			break;
+			case 3:
+				strcpy(msg, "Mensagens removidas:");
+				
+				puts("Usuario (mÃ¡ximo 19 caracteres):");
+				fpurge(stdin);
+				fgets(envio.name, NAME_SIZE, stdin);
+				strtok(envio.name, "\n");
+				envio.type = 3;
+			break;
+			case 4:
+				envio.type = 0;
+			break;
+			default:
+				puts("Coloque um valor vÃ¡lido!");
+				fpurge(stdin);
+			break;
+		}
 
-    printf("Cliente terminou com sucesso.\n");
-    exit(0);
+		// Finalizacao do programa
+		if(!envio.type){
+			close(s);
+			puts("Ate logo!");
+			exit(0);
+		}
+			
+		/* Envia a mensagem no buffer de envio para o servidor */
+		if (send(s, &envio, sizeof(envio), 0) < 0) {
+			perror("Send()");
+			exit(5);
+		}
 
+		/* Recebe a mensagem do servidor no buffer de recepï¿½ï¿½o */
+		if (recv(s, &envio, sizeof(envio), 0) < 0) {
+			perror("Recv()");
+			exit(6);
+		}
+		
+		// print resultado:
+		if(envio.type != 0)
+			printf("%s %i\n\n", msg, envio.type);
+			
+		printf("%s\n", envio.answer);
+		
+		/*Limpando pra proxima*/
+		strcpy(envio.name, "empty");
+		strcpy(envio.message, "empty");
+		strcpy(envio.answer, "\0");
+	}
+	
+	/* Fecha o socket */
+	close(s);
+
+	printf("Cliente terminou com sucesso.\n");
+	exit(0);
 }
-
-
--- Tarefa2/servtcp.c
+++ Tarefa2/servtcp.c
@@ -4,26 +4,93 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
 
-/*Variavies do programa
-*/
-#define NAME_SIZE 19
-#define MG_SIZE 79
+/* Constantes do programa */
+#define NAME_SIZE 20
+#define MSG_SIZE 80
+#define LIST_SIZE 10
+#define ANS_SIZE 1250
 
 /*
 * Struct to save Mensagens
 */
 
-struct msg_list {
-	char name_list [NAME_SIZE];
-	char mensage_list [MG_SIZE];
-} ;
-
-struct msg {
+typedef struct Msg {
 	char name [NAME_SIZE];
-	char mensage [MG_SIZE];
+	char message [MSG_SIZE];
+	char answer [ANS_SIZE];
 	int type;
-}msg;
+}Msg;
+
+//funÃ§Ãµes auxiliares
+
+//Procura e armazena nova msg
+int insertMsg(Msg list[], Msg *envio){
+
+	int i;
+
+	// procurando espaco vazio e atribuindo nova msg
+	for(i = 0; i < LIST_SIZE; i++){
+		if(strcmp(list[i].name, "") == 0){
+			list[i] = *envio;
+			strcpy(envio->answer, "Mensagem incluida com sucesso!");
+			envio->type = 0;
+			return 1;
+		}
+	}
+
+	strcpy(envio->answer, "A mensagem nao foi incluida. A lista ja esta cheia!");
+	return 0;
+}
+
+int removeMsg(Msg list[], int pList, Msg *envio){
+	
+	int i, rmvdMsgs = 0;
+	char res[1200] = "";
+	
+	for(i = 0; i < LIST_SIZE; i++){
+		if(strcmp(list[i].name, envio->name) == 0){
+			
+			// salvando valores
+			strcat(res, "Usuario:");
+			strcat(res, list[i].name);
+			strcat(res, " | ");
+			strcat(res, "Mensagem:");
+			strcat(res, list[i].message);
+			
+			//removendo
+			strcpy(list[i].name, "\0");
+			rmvdMsgs++;
+		}
+	}
+	
+	strcpy(envio->answer, res);
+	if(rmvdMsgs == 0)
+		strcat(envio->answer, "Nao existem mensagens desse usuario.");
+	envio->type = rmvdMsgs;
+	return rmvdMsgs;
+}
+
+int getMsgs(Msg list[], int pList, Msg *envio){
+
+	char res[ANS_SIZE] = {""};
+	int i, count = 0;
+
+	for(i = 0; i < LIST_SIZE; i++){
+		if(strcmp(list[i].name, "\0") != 0){
+			strcat(res, "Usuario: ");
+			strcat(res, list[i].name);
+			strcat(res, "  |  ");
+			strcat(res, "Mensagem: ");
+			strcat(res, list[i].message);
+			count++;
+		}
+	}
+	
+	envio->type = count;
+	strcpy(envio->answer, res);
+}
 
 /*
  * Servidor TCP
@@ -32,23 +99,22 @@ void main(argc, argv)
 int argc;
 char **argv;
 {
-    unsigned short port;       
-    char sendbuf[12];              
-    char recvbuf[12];              
-    struct sockaddr_in client; 
-    struct sockaddr_in server; 
-    int s;			/* Socket para aceitar conexões       */
+    unsigned short port;
+    char sendbuf[12];
+    char recvbuf[12];
+    struct sockaddr_in client;
+    struct sockaddr_in server;
+    int s;			/* Socket para aceitar conexï¿½es       */
     int ns;			/* Socket conectado ao cliente        */
-    int namelen;      
-    struct msg_list list[10];	/* list of mensagens			*/
-    struct msg envio;
-
+    int namelen;
+    Msg list[10] = {"\0"};	/* list of mensagens			*/
+    int pList = 0;
+    Msg envio;
     /*
-     * O primeiro argumento (argv[1]) é a porta
-     * onde o servidor aguardará por conexões
+     * O primeiro argumento (argv[1]) ï¿½ a porta
+     * onde o servidor aguardarï¿½ por conexï¿½es
      */
-    if (argc != 2)
-    {
+    if (argc != 2) {
         fprintf(stderr, "Use: %s porta\n", argv[0]);
         exit(1);
     }
@@ -56,79 +122,90 @@ char **argv;
     port = (unsigned short) atoi(argv[1]);
 
     /*
-     * Cria um socket TCP (stream) para aguardar conexões
+     * Cria um socket TCP (stream) para aguardar conexï¿½es
      */
-    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-    {
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Socket()");
         exit(2);
     }
 
    /*
-    * Define a qual endereço IP e porta o servidor estará ligado.
+    * Define a qual endereï¿½o IP e porta o servidor estarï¿½ ligado.
     * IP = INADDDR_ANY -> faz com que o servidor se ligue em todos
-    * os endereços IP
+    * os endereï¿½os IP
     */
-    server.sin_family = AF_INET;   
-    server.sin_port   = htons(port);       
+    server.sin_family = AF_INET;
+    server.sin_port   = htons(port);
     server.sin_addr.s_addr = INADDR_ANY;
 
     /*
-     * Liga o servidor à porta definida anteriormente.
+     * Liga o servidor ï¿½ porta definida anteriormente.
      */
-    if (bind(s, (struct sockaddr *)&server, sizeof(server)) < 0)
-    {
+    if (bind(s, (struct sockaddr *)&server, sizeof(server)) < 0) {
        perror("Bind()");
        exit(3);
     }
 
     /*
-     * Prepara o socket para aguardar por conexões e
-     * cria uma fila de conexões pendentes.
+     * Prepara o socket para aguardar por conexï¿½es e
+     * cria uma fila de conexï¿½es pendentes.
      */
-    if (listen(s, 1) != 0)
-    {
+    if (listen(s, 1) != 0) {
         perror("Listen()");
         exit(4);
     }
 
     /*
-     * Aceita uma conexão e cria um novo socket através do qual
-     * ocorrerá a comunicação com o cliente.
+     * Aceita uma conexï¿½o e cria um novo socket atravï¿½s do qual
+     * ocorrerï¿½ a comunicaï¿½ï¿½o com o cliente.
      */
     namelen = sizeof(client);
-    if ((ns = accept(s, (struct sockaddr *)&client, &namelen)) == -1)
-    {
+    if ((ns = accept(s, (struct sockaddr *)&client, &namelen)) == -1) {
         perror("Accept()");
         exit(5);
     }
 
-    /* Recebe uma mensagem do cliente através do novo socket conectado */
-    if (recv(ns, &envio, sizeof(envio), 0) == -1)
-    {
-        perror("Recv()");
-        exit(6);
-    }
-    printf("Mensagem recebida do cliente: %s\n", envio.mensage);
+	while(1) {
+	    /* Recebe uma mensagem do cliente atravï¿½s do novo socket conectado */
+	    if (recv(ns, &envio, sizeof(envio), 0) == -1) {
+	        perror("Recv()");
+	        exit(6);
+	    }
+	    puts("Mensagem recebida do cliente:");
+		printf("Tipo: %i | Nome: %s | Msg: %s\n", envio.type, envio.name, envio.message);
 
-    strcpy(envio.mensage, "Resposta");
-    
-    /* Envia uma mensagem ao cliente através do socket conectado */
-    if (send(ns, &envio, sizeof(envio), 0) < 0)
-    {
-        perror("Send()");
-        exit(7);
-    }
-    printf("Mensagem enviada ao cliente: %s\n", envio.mensage);
+		switch(envio.type) {
+			case 1:
+				if(insertMsg(list, &envio) == 1);
+					pList++;	
+			break;
+			case 2:
+				getMsgs(list, pList, &envio);
+			break;
+			case 3:
+				pList -= removeMsg(list, pList, &envio);
+			break;
+		}
+
+	    /* Envia uma mensagem ao cliente atravï¿½s do socket conectado */
+	    if (send(ns, &envio, sizeof(envio), 0) < 0) {
+	        perror("Send()");
+	        exit(7);
+	    }
+	    printf("Mensagem enviada ao cliente: %s\n", envio.answer);
+	    /*Limpando pra proxima*/
+	    strcpy(envio.name, " ");
+	    strcpy(envio.message, " ");
+	    strcpy(envio.answer, " ");
+	    envio.type = 0;
+	}
 
     /* Fecha o socket conectado ao cliente */
     close(ns);
 
-    /* Fecha o socket aguardando por conexões */
+    /* Fecha o socket aguardando por conexï¿½es */
     close(s);
 
     printf("Servidor terminou com sucesso.\n");
     exit(0);
 }
-
-
